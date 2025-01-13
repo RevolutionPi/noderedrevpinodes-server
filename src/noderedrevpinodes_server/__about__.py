a61d72f36a5e27f764a991d6640e53ca0c3bd1da
Sven Sager <s.sager@kunbus.com>
2025-01-13 09:37:22 +0100

    chore: Set version to 1.2.0
    Signed-off-by: Sven Sager <s.sager@kunbus.com>

--- src/noderedrevpinodes_server/__about__.py
+++ src/noderedrevpinodes_server/__about__.py
@@ -3,6 +3,6 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 """Metadata of package."""
 __author__ = "erminas GmbH, KUNBUS GmbH"
-__copyright__ = "Copyright (C) 2019-2023 erminas GmbH, 2024 KUNBUS GmbH"
+__copyright__ = "Copyright (C) 2019-2023 erminas GmbH, 2025 KUNBUS GmbH"
 __license__ = "LGPL-3.0-only"
-__version__ = "1.1.2"
+__version__ = "1.2.0"
